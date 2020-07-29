854762ac2a276ffbf8f4e27d772580ba130a92d1
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-29 10:33:53 +0100

    Updates

---
+++ precache-manifest.7271738bf07107767848510bd4c89662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354eb69c736fb033803f72542763787a",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "92739df6dc892d041ee7",
+    "url": "/react-todo-list/static/css/main.6b3282fb.chunk.css"
+  },
+  {
+    "revision": "09179f08cf3c3e572c82",
+    "url": "/react-todo-list/static/js/2.5eff9af4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.5eff9af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92739df6dc892d041ee7",
+    "url": "/react-todo-list/static/js/main.b0611e50.chunk.js"
+  },
+  {
+    "revision": "80682d5f29ce18e87584",
+    "url": "/react-todo-list/static/js/runtime-main.28be1446.js"
+  }
+]);
