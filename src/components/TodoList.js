f29fe5f82e839db5b646a0f4aa91b3d43d709c03
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-29 10:29:04 +0100

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+  return (
+    <div className="list-wrapper">
+      {props.tasks.map((todo, index) => <Todo content={todo} key={index} id={index} onDelete={props.onDelete} />)}
+    </div>
+  );
+};
+
+export default TodoList;
